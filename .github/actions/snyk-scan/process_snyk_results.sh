13d1244814cc02b6115aa08103e534f0053758fc
s99mj <sandhya.gandalwar@justice.gov.uk>
2026-05-06 00:16:28 +0100

    updated formatting

--- .github/actions/snyk-scan/process_snyk_results.sh
+++ .github/actions/snyk-scan/process_snyk_results.sh
@@ -98,14 +98,6 @@ process_sarif() {
       (.locations[0].logicalLocations[0].fullyQualifiedName
         // .locations[0].physicalLocation.artifactLocation.uri
         // "unknown");
-    def result_key($r):
-      (
-        ($r.ruleId // "issue") + "|"
-        + (($r.properties.severity // $r.level // "unknown") | tostring) + "|"
-        + (($r.locations[0].logicalLocations[0].fullyQualifiedName
-            // $r.locations[0].physicalLocation.artifactLocation.uri
-            // "unknown") | tostring)
-      );
     def find_vuln($id; $component):
       (
         all_vulns
@@ -124,51 +116,55 @@ process_sarif() {
       ) | unique;
 
     . as $sarif
-    | [
-        .runs[0].results[]? as $r
-        | ($r.ruleId // "issue") as $id
-        | ($r | component_of_result) as $component
-        | (find_vuln($id; $component)) as $v
-        | if $v == null then
-            $r
-          elif (($r.message.text // "") | contains(" | Package:")) then
-            $r
-          else
-            (
-              " | Package: " + (package_of($v))
-              + " | CVE: " + (if (cves_of($v)) == "" then "n/a" else cves_of($v) end)
-              + " | CWE: " + (if (cwes_of($v)) == "" then "n/a" else cwes_of($v) end)
-              + " | GHSA: " + (if (ghsas_of($v)) == "" then "n/a" else ghsas_of($v) end)
-              + " | Disclosure: " + (if (disclosure_of($v)) == "" then "n/a" else disclosure_of($v) end)
-              + " | Ref: https://security.snyk.io/vuln/" + $id
-            ) as $extra
-            | $r
-            | .message.text = ((.message.text // "") + $extra)
-          end
-      ] as $existing_results
-    | [
-        all_vulns[]?
-        | (vuln_sev(.)) as $sev
-        | {
-            ruleId: (.id // "issue"),
-            level: (level_of($sev)),
-            message: { text: (.title // .id // "Snyk vulnerability") },
-            locations: [
-              {
-                logicalLocations: [
+    | .runs[0].results = (
+        if ((.runs[0].results // []) | length) > 0 then
+          [
+            .runs[0].results[]? as $r
+            | ($r.ruleId // "issue") as $id
+            | ($r | component_of_result) as $component
+            | (find_vuln($id; $component)) as $v
+            | if $v == null then
+                $r
+              elif (($r.message.text // "") | contains(" | Package:")) then
+                $r
+              else
+                (
+                  " | Package: " + (package_of($v))
+                  + " | CVE: " + (if (cves_of($v)) == "" then "n/a" else cves_of($v) end)
+                  + " | CWE: " + (if (cwes_of($v)) == "" then "n/a" else cwes_of($v) end)
+                  + " | GHSA: " + (if (ghsas_of($v)) == "" then "n/a" else ghsas_of($v) end)
+                  + " | Disclosure: " + (if (disclosure_of($v)) == "" then "n/a" else disclosure_of($v) end)
+                  + " | Ref: https://security.snyk.io/vuln/" + $id
+                ) as $extra
+                | $r
+                | .message.text = ((.message.text // "") + $extra)
+              end
+          ]
+        else
+          [
+            all_vulns[]?
+            | (vuln_sev(.)) as $sev
+            | {
+                ruleId: (.id // "issue"),
+                level: (level_of($sev)),
+                message: { text: (.title // .id // "Snyk vulnerability") },
+                locations: [
                   {
-                    fullyQualifiedName: package_of(.)
+                    logicalLocations: [
+                      {
+                        fullyQualifiedName: package_of(.)
+                      }
+                    ]
                   }
-                ]
+                ],
+                properties: {
+                  severity: $sev,
+                  tags: (["severity:" + $sev] + ui_tags(.))
+                }
               }
-            ],
-            properties: {
-              severity: $sev,
-              tags: (["severity:" + $sev] + ui_tags(.))
-            }
-          }
-      ] as $generated_results
-    | .runs[0].results = (($existing_results + $generated_results) | unique_by(result_key(.)))
+          ]
+        end
+      )
     | (
         [
           all_vulns[]?
